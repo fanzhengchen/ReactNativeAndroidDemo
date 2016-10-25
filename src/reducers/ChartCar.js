f021aeff6a9a1a399de09258a3d511d87ddf14b5
markfan <fanzhengchen@gmail.com>
2016-10-25 09:53:36 +0800

    immutable

--- src/reducers/ChartCar.js
+++ src/reducers/ChartCar.js
@@ -2,23 +2,25 @@
  * Created by fanzhengchen on 10/24/16.
  */
 import * as types from '../constants/actionTypes';
+var Immutable = require('Immutable');
 
-export default function listData(state = {
-    fuck: 'fuck',
-}, action) {
+export default function listData(state =
+                                     Immutable.List([
+                                         Immutable.Map({
+                                             flag: 0,
+                                         }),
+                                         Immutable.Map(
+                                             {
+                                                 flag: 1,
+                                             }),
+                                     ])
+    , action) {
     console.log("getList " + types.CHART_CAR_CHECK + " " + action.type + " " + JSON.stringify(state));
     switch (action.type) {
         case types.CHART_CAR_CHECK: {
-            // var {flag, text}= state[action.index];
-            // flag ^= 1;
-            // state[action.index] = {
-            //     flag: flag,
-            //     text: text,
-            // };
             console.log("action index %d", action.index);
-            var {fuck} = {...state};
-            var text = fuck;
-            return {fuck: text + " " + text};
+            var {flag} = state[0];
+            return {flag: flag + flag};
         }
         default:
             return state;
