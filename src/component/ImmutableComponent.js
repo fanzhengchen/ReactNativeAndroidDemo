bd3526cebfe91231e40fe13df9e51ca111c5ef38
fanzhengchen <fanzhengchen@gmail.com>
2016-11-03 14:36:32 +0800

    swipe list view

--- src/component/ImmutableComponent.js
+++ src/component/ImmutableComponent.js
@@ -18,10 +18,22 @@ import {
 
 import RxActions from '../actions/RxActions';
 import {connect} from '../state/RxState';
+import {SwipeListView, SwipeRow} from 'react-native-swipe-list-view';
 class ImmutableComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+        this.state = {
+            basic: true,
+            listViewData: Array(20).fill('').map((_, i)=>`item #${i}`)
+        };
+    }
+
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>
                     ImmutableContainer fuck
                 </Text>
@@ -31,12 +43,53 @@ class ImmutableComponent extends Component {
                 <TouchableOpacity onPress={this._onPress.bind(this)}>
                     <Text>Press</Text>
                 </TouchableOpacity>
+                {
+
+                    this.state.basic &&
+                    <SwipeListView
+                        dataSource={this.ds.cloneWithRows(this.state.listViewData)}
+                        renderRow={ data => (
+                            <TouchableHighlight
+                                onPress={ _ => console.log('You touched me') }
+                                style={styles.rowFront}
+                                underlayColor={'#AAA'}
+                            >
+                                <View>
+                                    <Text>I'm {data} in a SwipeListView</Text>
+                                </View>
+                            </TouchableHighlight>
+                        )}
+                        renderHiddenRow={ (data, secId, rowId, rowMap) => (
+                            <View style={styles.rowBack}>
+                                <Text>Left</Text>
+                                <View style={[styles.backRightBtn, styles.backRightBtnLeft]}>
+                                    <Text style={styles.backTextWhite}>Right</Text>
+                                </View>
+                                <TouchableOpacity style={[styles.backRightBtn, styles.backRightBtnRight]}
+                                                  onPress={ _ => this.deleteRow(secId, rowId, rowMap) }>
+                                    <Text style={styles.backTextWhite}>Delete</Text>
+                                </TouchableOpacity>
+                            </View>
+                        )}
+                        leftOpenValue={75}
+                        rightOpenValue={-150}
+
+                    />
+                }
+
             </View>
-        )
+        );
     }
 
     _onPress() {
         RxActions.increment$.next(10);
+    }
+
+    deleteRow(secId, rowId, rowMap) {
+        rowMap[`${secId}${rowId}`].closeRow();
+        const newData = [...this.state.listViewData];
+        newData.splice(rowId, 1);
+        this.setState({listViewData: newData});
     }
 }
 export default connect(state => {
@@ -44,3 +97,78 @@ export default connect(state => {
         counter: state.counter,
     });
 })(ImmutableComponent);
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        flex: 1
+    },
+    standalone: {
+        marginTop: 30,
+        marginBottom: 30,
+    },
+    standaloneRowFront: {
+        alignItems: 'center',
+        backgroundColor: '#CCC',
+        justifyContent: 'center',
+        height: 50,
+    },
+    standaloneRowBack: {
+        alignItems: 'center',
+        backgroundColor: '#8BC645',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 15
+    },
+    backTextWhite: {
+        color: '#FFF'
+    },
+    rowFront: {
+        alignItems: 'center',
+        backgroundColor: '#CCC',
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        justifyContent: 'center',
+        height: 50,
+    },
+    rowBack: {
+        alignItems: 'center',
+        backgroundColor: '#DDD',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingLeft: 15,
+    },
+    backRightBtn: {
+        alignItems: 'center',
+        bottom: 0,
+        justifyContent: 'center',
+        position: 'absolute',
+        top: 0,
+        width: 75
+    },
+    backRightBtnLeft: {
+        backgroundColor: 'blue',
+        right: 75
+    },
+    backRightBtnRight: {
+        backgroundColor: 'red',
+        right: 0
+    },
+    controls: {
+        alignItems: 'center',
+        marginBottom: 30
+    },
+    switchContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginBottom: 5
+    },
+    switch: {
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: 'black',
+        paddingVertical: 10,
+        width: 100,
+    }
+});
---
+++ src/component/ListComponent.js
@@ -0,0 +1,27 @@
+/**
+ * Created by mark on 16-11-2.
+ */
+import React, {
+    Component
+} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableHighlight,
+    TouchableOpacity,
+    DrawerLayoutAndroid,
+    ToolbarAndroid,
+    Navigator,
+    ListView,
+} from 'react-native';
+
+export default class ListComponent extends Component {
+
+
+    render() {
+        return (
+            <Text>Fuck</Text>
+        )
+    }
+}
